9a7b077114bbb29cd8cc38bf88296dc15c9ad5a6
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-10-13 15:10:02 +0200

    Initial rename.

--- build/failOnFirstBuild.sh
+++ build/failOnFirstBuild.sh
@@ -2,7 +2,7 @@
 
 echo "${GITHUB_REPOSITORY}"
 echo "${DOCKER_SERVICE}"
-if [ "${GITHUB_REPOSITORY}" != "KvalitetsIT/kithugs" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/kithugs" ]; then
+if [ "${GITHUB_REPOSITORY}" != "KvalitetsIT/medcom-vdx-healthcheck" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/medcom-vdx-healthcheck" ]; then
   echo "Please run setup.sh REPOSITORY_NAME"
   exit 1
 fi
