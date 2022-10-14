f3d6574e26de588f9ba0c2570a1c0e403c0d062e
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-10-14 07:55:53 +0200

    Remove database dependency

--- service/src/main/java/dk/medcom/healthcheck/configuration/HealthcheckConfiguration.java
+++ service/src/main/java/dk/medcom/healthcheck/configuration/HealthcheckConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class HelloConfiguration{
+public class HealthcheckConfiguration {
     @Bean
     public HelloService helloService() {
         return new HelloServiceImpl();
--- service/src/main/java/dk/medcom/healthcheck/dao/entity/HelloEntity.java
+++
@@ -1,7 +0,0 @@
-package dk.medcom.healthcheck.dao.entity;
-
-public record HelloEntity(Long id, String name) {
-    public static HelloEntity createInstance(String name) {
-        return new HelloEntity(null, name);
-    }
-}
