75f3ff1a2b409fb939ae92bfcd9b57dacd7aa5ae
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-10-14 08:16:38 +0200

    Completely remove mariadb.

--- integrationtest/src/test/java/dk/medcom/healthcheck/integrationtest/ServiceStarter.java
+++ integrationtest/src/test/java/dk/medcom/healthcheck/integrationtest/ServiceStarter.java
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
@@ -18,7 +17,6 @@ import java.util.Collections;
 public class ServiceStarter {
     private static final Logger logger = LoggerFactory.getLogger(ServiceStarter.class);
     private static final Logger serviceLogger = LoggerFactory.getLogger("medcom-vdx-healthcheck");
-    private static final Logger mariadbLogger = LoggerFactory.getLogger("mariadb");
 
     private Network dockerNetwork;
     private String jdbcUrl;
@@ -26,19 +24,11 @@ public class ServiceStarter {
     public void startServices() {
         dockerNetwork = Network.newNetwork();
 
-        setupDatabaseContainer();
-
-        System.setProperty("JDBC.URL", jdbcUrl);
-        System.setProperty("JDBC.USER", "hellouser");
-        System.setProperty("JDBC.PASS", "secret1234");
-
         SpringApplication.run((VideoLinkHandlerApplication.class));
     }
 
     public GenericContainer startServicesInDocker() {
         dockerNetwork = Network.newNetwork();
-
-        setupDatabaseContainer();
 
         var resourcesContainerName = "medcom-vdx-healthcheck-resources";
         var resourcesRunning = containerRunning(resourcesContainerName);
@@ -63,10 +53,6 @@ public class ServiceStarter {
 
                 .withEnv("LOG_LEVEL", "INFO")
 
-                .withEnv("JDBC_URL", "jdbc:mariadb://mariadb:3306/hellodb")
-                .withEnv("JDBC_USER", "hellouser")
-                .withEnv("JDBC_PASS", "secret1234")
-
                 .withEnv("spring.flyway.locations", "classpath:db/migration,filesystem:/app/sql")
                 .withClasspathResourceMapping("db/migration/V901__extra_data_for_integration_test.sql", "/app/sql/V901__extra_data_for_integration_test.sql", BindMode.READ_ONLY)
 //                .withEnv("JVM_OPTS", "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8000")
@@ -87,19 +73,6 @@ public class ServiceStarter {
                 .withNameFilter(Collections.singleton(containerName))
                 .exec()
                 .size() != 0;
-    }
-
-    private void setupDatabaseContainer() {
-        // Database server for Organisation.
-        MariaDBContainer mariadb = (MariaDBContainer) new MariaDBContainer<>("mariadb:10.6")
-                .withDatabaseName("hellodb")
-                .withUsername("hellouser")
-                .withPassword("secret1234")
-                .withNetwork(dockerNetwork)
-                .withNetworkAliases("mariadb");
-        mariadb.start();
-        jdbcUrl = mariadb.getJdbcUrl();
-        attachLogger(mariadbLogger, mariadb);
     }
 
     private void attachLogger(Logger logger, GenericContainer container) {
