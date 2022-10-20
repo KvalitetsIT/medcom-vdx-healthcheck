16ae92a8d89fb9d4818e029014845ca232194a98
Jonas Pedersen <jpe@kvalitetsit.dk>
2022-10-20 12:48:02 +0200

    Web part. (#13)

---
+++ service/src/main/java/dk/medcom/healthcheck/controller/HtmlController.java
@@ -0,0 +1,66 @@
+package dk.medcom.healthcheck.controller;
+
+import dk.medcom.healthcheck.controller.model.Status;
+import dk.medcom.healthcheck.service.HealthcheckService;
+import dk.medcom.healthcheck.service.model.HealthcheckResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class HtmlController {
+    private static final Logger logger = LoggerFactory.getLogger(HtmlController.class);
+    private final HealthcheckService healthcheckService;
+
+    public HtmlController(HealthcheckService healthcheckService) {
+        this.healthcheckService = healthcheckService;
+    }
+
+    @RequestMapping(path = "/")
+    public ModelAndView get() {
+        logger.info("Showing main page.");
+
+        var response = new ModelAndView();
+        response.setViewName("index");
+
+        return response;
+    }
+
+
+    @RequestMapping(path = "/execute")
+    public ModelAndView execute() {
+        logger.info("Showing main page.");
+
+        var result = healthcheckService.checkHealth();
+
+        var response = new ModelAndView();
+        response.setViewName("result");
+        response.addObject("allOk", allOk(result));
+        response.addObject("status", createStatus(result));
+
+        return response;
+    }
+
+    private List<Status> createStatus(HealthcheckResult result) {
+        return Arrays.asList(createStatus("STS", result.sts()),
+                createStatus("VideoAPI", result.videoAPi()),
+                createStatus("Access Token For VideoAPI", result.accessTokenForVideoApi()),
+                createStatus("ShortLink", result.shortLink()));
+    }
+
+    private Status createStatus(String name, dk.medcom.healthcheck.service.model.Status status) {
+        return new Status(name, status.ok(), status.responseTime(), status.message());
+    }
+
+    private boolean allOk(HealthcheckResult result) {
+        return result.sts().ok() &&
+                result.videoAPi().ok() &&
+                result.shortLink().ok() &&
+                result.accessTokenForVideoApi().ok();
+    }
+}
